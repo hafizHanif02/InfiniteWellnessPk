ce76e2f905445e02973b9d36c665734ac60b8b2b
hafizHanif02 <hafiz.hanif992@gmail.com>
2023-09-21 20:54:29 +0500

    item db fixed

--- database/migrations/2020_08_26_101134_create_items_table.php
+++ database/migrations/2020_08_26_101134_create_items_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('itemcategory_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('item_category_id')->constrained()->cascadeOnDelete();
             $table->bigInteger('unit');
             $table->text('description')->nullable();
             $table->integer('available_quantity')->default(0);
