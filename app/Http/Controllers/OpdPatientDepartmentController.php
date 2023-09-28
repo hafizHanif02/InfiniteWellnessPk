e01a468f44e9186e0bc950152daac4df3ec7e468
hafizHanif02 <hafiz.hanif992@gmail.com>
2023-09-28 16:53:12 +0500

    Update OpdPatientDepartmentController.php

--- app/Http/Controllers/OpdPatientDepartmentController.php
+++ app/Http/Controllers/OpdPatientDepartmentController.php
@@ -79,8 +79,8 @@ class OpdPatientDepartmentController extends AppBaseController
             $data['last_visit'] = OpdPatientDepartment::findOrFail($id);
         }
 
-        foreach ($data['patients'] as $key => $value) {
-            $data['patients'][$key] = $key. " - ".$value;
+        foreach ($data['patients2'] as $key => $value) {
+            $data['patients'][$value->id] = $value->MR. " - ".$value->patientUser->full_name;
         }
 
 
