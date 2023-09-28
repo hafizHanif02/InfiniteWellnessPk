e4f76f3b7335160df111870d992caacbfdd9c3de
hafizHanif02 <hafiz.hanif992@gmail.com>
2023-09-28 17:08:43 +0500

    Update NewStockController.php

--- app/Http/Controllers/NewStockController.php
+++ app/Http/Controllers/NewStockController.php
@@ -91,6 +91,7 @@ class NewStockController extends Controller
                     ])->update([
                         'selling_price' => $transferProduct->product->unit_trade,
                         'buying_price' => $transferProduct->product->cost_price,
+                        'barcode' => $item->barcode,
                     ]);
                 } else {
                     $brands = Brand::where('name',$transferProduct->product->manufacturer->company_name)->first();
@@ -101,7 +102,7 @@ class NewStockController extends Controller
                             'brand_id' => $brands->id,
                             'name' => $transferProduct->product->product_name,
                             'generic_formula' => $transferProduct->product->generic->formula,
-                            'barcode' => $transferProduct->product->barcode,
+                            'barcode' => $item->barcode,
                             'selling_price' => $transferProduct->product->unit_trade,
                             'buying_price' => $transferProduct->product->cost_price,
                             'description' => $transferProduct->product->package_detail,
