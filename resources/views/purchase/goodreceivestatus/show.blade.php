cffafdc09fd717e9342c830b3ba76342faa859f0
Hafiz Hanif <hafiz.hanif992@gmail.com>
2023-10-02 16:30:09 -0700

    Update show.blade.php

--- resources/views/purchase/goodreceivestatus/show.blade.php
+++ resources/views/purchase/goodreceivestatus/show.blade.php
@@ -4,8 +4,12 @@
             <div class="card-body">
                 <div class="d-flex justify-content-between">
                     <h3>Good Receive Status Detail</h3>
-                    <a href="{{ route('purchase.good-receive-statuses.index') }}"
-                        class="btn btn-secondary float-end mr-5 mb-3">Back</a>
+                    <div class="d-flex gap-5">
+                        <a href="{{ route('purchase.good_receive_note.print', $goodReceiveNote->id) }}"
+                            class="btn btn-primary float-end mr-5 mb-3">Print</a>
+                        <a href="{{ route('purchase.good-receive-statuses.index') }}"
+                            class="btn btn-secondary float-end mr-5 mb-3">Back</a>
+                        </div>
                 </div>
                 <table class="table table-bordered table-hover text-start">
                     <tbody>
