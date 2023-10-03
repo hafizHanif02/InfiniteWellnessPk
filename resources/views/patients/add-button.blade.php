32271d3f3de9e87ac53929cf2767375d33cc4253
Hafiz Hanif <hafiz.hanif992@gmail.com>
2023-10-02 21:14:29 -0700

    Update add-button.blade.php

--- resources/views/patients/add-button.blade.php
+++ resources/views/patients/add-button.blade.php
@@ -1,5 +1,5 @@
 <div class="dropdown">
-    @role('Admin|Receptionist')
+    
     <a href="#" class="btn btn-primary" id="dropdownMenuButton" data-bs-toggle="dropdown"
        aria-haspopup="true" aria-expanded="false">{{ __('messages.common.actions') }}
         <i class="fas fa-chevron-down"></i>
@@ -14,6 +14,5 @@
                class="dropdown-item  px-5" target="_blank"  data-tirbo='false'>{{ __('messages.common.export_to_excel') }}</a>
         </li>
     </ul>
-    @endrole
 </div>
 
