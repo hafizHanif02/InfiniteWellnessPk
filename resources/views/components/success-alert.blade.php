de9bd3c94c1938eaa39e472f3a241e1d82d60533
hafizHanif02 <hafiz.hanif992@gmail.com>
2023-10-03 12:41:09 +0500

    alert session update

--- resources/views/components/success-alert.blade.php
+++ resources/views/components/success-alert.blade.php
@@ -4,7 +4,7 @@
         toast: true,
         position: 'top-end',
         showConfirmButton: false,
-        timer: 3000,
+        timer: 10000,
         timerProgressBar: true,
         didOpen: (toast) => {
             toast.addEventListener('mouseenter', Swal.stopTimer)
