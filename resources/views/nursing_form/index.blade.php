ec90e745028771575812e5f4e1504399dc96fd5e
Hafiz Hanif <hafiz.hanif992@gmail.com>
2023-10-04 18:10:21 -0700

    Update index.blade.php

--- resources/views/nursing_form/index.blade.php
+++ resources/views/nursing_form/index.blade.php
@@ -6,9 +6,11 @@
 <div class="container-fluid">
     <div class="d-flex flex-column">
         @include('flash::message')
+        @role('Admin|Nurse|Doctor|Receptionist')
         <div class="col-md-12 mb-5 text-end">
             <a href="{{ route('nursing-form.create') }}" target="_blank"><button class="btn btn-primary">Add New Nursing Form</button></a>
         </div>
+        @endrole
         <div class="table-responsive">
                 <table class="table table-striped">
                     <thead>
