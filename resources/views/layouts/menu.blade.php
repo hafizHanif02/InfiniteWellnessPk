50755db93116054b6892d9b2aadcff7743f33bcf
Hafiz Hanif <hafiz.hanif992@gmail.com>
2023-10-04 17:35:31 -0700

    Update menu.blade.php

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -562,6 +562,17 @@ $vaccinationsPatient = getMenuLinks(\App\Models\User::MAIN_VACCINATION_MGT)
     </li>
     @endmodule
 
+    {{-- dietitian-form --}}
+
+
+    <li class="nav-item  {{ Request::is('dietitian-form*') ? 'active' : '' }}">
+        <a class="nav-link  d-flex align-items-center py-3"
+           href="{{ route('dietitan.index') }}">
+            <span class="aside-menu-icon pe-3 pe-3"><i class="fas fa-tint"></i></span>
+            <span class="aside-menu-title">Dietitan Assessment</span>
+        </a>
+    </li>
+    
     @module('Doctors',$modules)
     <li class="nav-item  {{ Request::is('employee/doctor*','prescriptions*','schedules*','doctors*') ? 'active' : '' }}">
         <a class="nav-link  d-flex align-items-center py-3"
@@ -1119,7 +1130,7 @@ $vaccinationsPatient = getMenuLinks(\App\Models\User::MAIN_VACCINATION_MGT)
             <span class="aside-menu-title">Dietitan Assessment</span>
         </a>
     </li>
-    
+
     @module('Notice Boards',$modules)
     <li class="nav-item  {{ Request::is('employee/notice-board*') ? 'active' : '' }}">
         <a class="nav-link  d-flex align-items-center py-3"
