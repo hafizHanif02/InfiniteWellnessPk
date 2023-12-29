4b01c58c4440debf909093d7b07d2444871412fd
Hammad <hw13604@gmail.com>
2023-12-30 03:56:11 +0500

    GRN Payments Set !!

--- resources/views/expenses/GRNPayments/create.blade.php
+++ resources/views/expenses/GRNPayments/create.blade.php
@@ -62,20 +62,24 @@
                             <div class="row mt-5">
                                 <div class="col-md-6">
                                     <label class="form-label" for="paid_amount">Enter Amount</label>
-                                    <input type="number" step="any" class="form-control" name="paid_amount" id="paid_amount">
+                                    <input type="number" step="any" class="form-control" name="paid_amount"
+                                        id="paid_amount">
+                                    <span id="paid_amount_error"></span>
                                 </div>
                                 <div class="col-md-6">
                                     <label class="form-label" for="paid_date">Enter Date</label>
                                     <input type="date" class="form-control" name="paid_date" id="paid_date">
+                                    <span id="paid_date_error"></span>
                                 </div>
                                 <div class="col-md-12 mt-5">
                                     <label class="form-label" for="paid_date">Enter Date</label>
                                     <textarea class="form-control" name="comments" id="comments" cols="30" rows="10"></textarea>
+                                    <span id="comments_error"></span>
                                 </div>
                             </div>
                         </div>
                         <div class="d-flex justify-content-center mb-5 mt-5">
-                            <button class="btn btn-primary">Submit</button>
+                            <button class="btn btn-primary" id="submitBtn">Submit</button>
                         </div>
                 </div>
             </div>
@@ -84,9 +88,26 @@
     </div>
     <script>
         $(document).ready(function() {
+
             $('#grn_id').select2();
+
+            $("#submitBtn").attr("disabled", true);
+
             $("#grn_id").change(function() {
+
+                $("#paid_amount").val("");
+                $("#paid_date").val("");
+                $("#comments").val("");
+
+                if ($("#paid_amount").val() !== '' && $("#paid_date").val() !== '' && $("#comments")
+                    .val() !== '') {
+                    $("#submitBtn").attr("disabled", false);
+                } else {
+                    $("#submitBtn").attr("disabled", true);
+                }
+
                 var id = $(this).val();
+
                 $.ajax({
                     type: "POST",
                     url: "/get-grn/",
@@ -106,8 +127,10 @@
                         }
 
                         RemainingAmount = response.grn.net_total_amount - paid_amount;
+                        RemainingAmount = RemainingAmount.toFixed(2);
+                        console.log(RemainingAmount);
 
-                        if(RemainingAmount == 0){
+                        if (RemainingAmount == 0) {
                             $("#paid_amount").attr("disabled", true);
                         }
 
@@ -118,12 +141,45 @@
                                 <td>${response.grn.invoice_number}</td>
                                 <td>${response.grn.net_total_amount}</td>
                                 <td>${paid_amount}</td>
-                                <td>${response.grn.net_total_amount - paid_amount}</td>
+                                <td>${RemainingAmount}</td>
                             </tr>
                         `);
                     }
                 })
+
+
+                $("#paid_amount").on("keyup", function() {
+                    if ($("#paid_amount").val() !== '' && $("#paid_date").val() !== '' && $(
+                            "#comments")
+                        .val() !== '') {
+                        $("#submitBtn").attr("disabled", false);
+                    } else {
+                        $("#submitBtn").attr("disabled", true);
+                    }
+                });
+
+                $("#paid_date").on("change", function() {
+                    if ($("#paid_amount").val() !== '' && $("#paid_date").val() !== '' && $(
+                            "#comments")
+                        .val() !== '') {
+                        $("#submitBtn").attr("disabled", false);
+                    } else {
+                        $("#submitBtn").attr("disabled", true);
+                    }
+                });
+
+                $("#comments").on("keyup", function() {
+                    if ($("#paid_amount").val() !== '' && $("#paid_date").val() !== '' && $(
+                            "#comments")
+                        .val() !== '') {
+                        $("#submitBtn").attr("disabled", false);
+                    } else {
+                        $("#submitBtn").attr("disabled", true);
+                    }
+                });
+
             });
+
         });
     </script>
 @endsection
